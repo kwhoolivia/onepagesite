e30c7fd581468e8f3723cac6cda63e8d4fc2aea0
kwhoolivia <113780948+kwhoolivia@users.noreply.github.com>
2022-11-25 19:54:48 +1100

    Create onesitepage.js

---
+++ onesitepage.js
@@ -0,0 +1,21 @@
+
+  function setup() {
+    createCanvas(2000, 200);
+  }
+  
+  function draw() {
+    
+  
+    if (keyIsPressed === true) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    rect(25, 30, 1350, 100);
+    describe('50-by-50 white rect that turns black on keypress.');
+
+
+
+  }
+
+
